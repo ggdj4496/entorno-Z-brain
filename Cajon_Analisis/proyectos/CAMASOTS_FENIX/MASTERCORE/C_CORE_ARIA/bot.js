5099aa5263fb115819093f262f340196712708da
ggdj4496 <260758552+ggdj4496@users.noreply.github.com>
2026-02-22 06:57:41 +0100

    COM

---
+++ Cajon_Analisis/proyectos/CAMASOTS_FENIX/MASTERCORE/C_CORE_ARIA/bot.js
@@ -0,0 +1,5 @@
+﻿const TelegramBot = require('node-telegram-bot-api');
+const token = '7891278144:AAHGf055z_4vO_2A_uP7Ff-D0O5R5h_A1_0';
+const bot = new TelegramBot(token, {polling: true});
+console.log('ARIA: Conectada. Esperando al Amo Leo...');
+bot.onText(/ARIA\?\?/, (msg) => { bot.sendMessage(msg.chat.id, 'dime'); });
